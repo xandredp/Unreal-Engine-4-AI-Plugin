59c6f952794ed126876ef7666e7406d98bdfff4c
Sena <sena0711@gmail.com>
2017-05-03 11:40:35 +1200

    Trimmeddown the fat. Plz build vs and pj file

--- Intermediate/Build/Win64/UE4Editor/Development/GameEnginePlugin/GameEnginePlugin.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_GAMEENGINEPLUGIN_H__
-#define __AUTO_GAMEENGINEPLUGIN_H__
-//Last Write: 11/04/2017 3:36:12 a.m. +00:00
-#include "D:\Users\Xandre.plo6323\Desktop\GameEnginePlugin\Source\GameEnginePlugin\GameEnginePlugin.h"
-#endif//__AUTO_GAMEENGINEPLUGIN_H__
--- Intermediate/Build/Win64/UE4Editor/Development/GameEnginePlugin/PCH.GameEnginePlugin.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:\Users\Xandre.plo6323\Desktop\GameEnginePlugin\Source\GameEnginePlugin\GameEnginePlugin.h"
