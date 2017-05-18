4b50c75b797c1190d29e1530cddae9500f10db42
MDS\Xandre.plo6323 <xduplooy@gmail.com>
2017-05-18 16:11:28 +1200

    TypeClass

---
+++ Plugins/AI_Plugin/Source/AI_Plugin/Private/TypeClass.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AI_PluginPrivatePCH.h"
+#include "AI_Plugin.h"
+#include "TypeClass.h"
+
+TypeClass::TypeClass()
+{
+}
+
+TypeClass::~TypeClass()
+{
+}
---
+++ Plugins/AI_Plugin/Source/AI_Plugin/Public/TypeClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AI_Plugin/Private/AI_PluginPrivatePCH.h"
+
+/**
+ * 
+ */
+class AI_PLUGIN_API TypeClass
+{
+public:
+	TypeClass();
+	~TypeClass();
+};
