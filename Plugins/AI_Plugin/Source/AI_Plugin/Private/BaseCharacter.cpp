09022981c9454bfd1be2bdac673201a672393db2
sena <sena0711@gmail.com>
2017-06-09 07:05:37 +1200

    Quick test -AI follwing theory  using blueprint

--- Plugins/AI_Plugin/Source/AI_Plugin/Private/BaseCharacter.cpp
+++ Plugins/AI_Plugin/Source/AI_Plugin/Private/BaseCharacter.cpp
@@ -9,8 +9,9 @@
 ABaseCharacter::ABaseCharacter()
 {
 	NoiseEmitterComp = CreateDefaultSubobject<UPawnNoiseEmitterComponent>(TEXT("NoiseEmitterComp"));
+
 	ArrowComp = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComp"));
-	ArrowComp->SetupAttachment(GetMesh());
+
 	Health = 100;
 	Stamina = 100;
 	bIsAlive = true;
